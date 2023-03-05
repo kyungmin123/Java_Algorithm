432c8bd491fa9bfcdf89ffe13e32aa7b73f0a4d7
kyungmin123 <77143146+kyungmin123@users.noreply.github.com>
2023-03-05 16:50:07 +0900

    [Bronze I] Title: 이항 계수 1, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(binomial(x, y));
+    }
+
+    public static int binomial(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binomial(n - 1, k) + binomial(n - 1,k - 1);
+    }
+}
